027d82893772a20613863a1c164a1a79a6c93ed6
Egoitz <eladron002@ikasle.ehu.eus>
2025-04-20 18:06:39 +0200

    Selector de hora de salida y entrada.

--- app/src/main/java/com/lksnext/ParkingELadron/view/fragment/CrearFragment.java
+++ app/src/main/java/com/lksnext/ParkingELadron/view/fragment/CrearFragment.java
@@ -1,6 +1,7 @@
 package com.lksnext.ParkingELadron.view.fragment;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,11 +11,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.lksnext.ParkingELadron.R;
 import com.lksnext.ParkingELadron.databinding.FragmentCrearBinding;
 import com.lksnext.ParkingELadron.viewmodel.CrearViewModel;
 
+import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -35,31 +39,85 @@ public class CrearFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.cvDia.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        binding.cvDia.setOnClickListener(v -> {
+            Calendar c = Calendar.getInstance();
+            int dia = c.get(Calendar.DAY_OF_MONTH);
+            int mes = c.get(Calendar.MONTH);
+            int year = c.get(Calendar.YEAR);
+            DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                    viewModel.setDate(new GregorianCalendar(i, i1, i2).getTime());
+                }
+            };
+            DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), listener, year, mes, dia);
+            datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+            Calendar maxDate = Calendar.getInstance();
+            maxDate.add(Calendar.DAY_OF_MONTH, 7);
+            datePickerDialog.getDatePicker().setMaxDate(maxDate.getTimeInMillis());
+            datePickerDialog.show();
+        });
+
+        binding.cvHoraInicio.setOnClickListener(v->{
+            Calendar c = Calendar.getInstance();
+            int hora = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+            TimePickerDialog.OnTimeSetListener listener = new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                    viewModel.setHoraInicio(String.format("%02d:%02d", i, i1));
+                }
+            };
+            TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(), listener, hora, minute, true);
+            timePickerDialog.show();
+        });
+
+        binding.cvHoraSalida.setOnClickListener(v->{
+            if(viewModel.getHoraInicio().getValue() != null) {
                 Calendar c = Calendar.getInstance();
-                int dia = c.get(Calendar.DAY_OF_MONTH);
-                int mes = c.get(Calendar.MONTH);
-                int year = c.get(Calendar.YEAR);
-                DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+                int hora = c.get(Calendar.HOUR_OF_DAY);
+                int minute = c.get(Calendar.MINUTE);
+                TimePickerDialog.OnTimeSetListener listener = new TimePickerDialog.OnTimeSetListener() {
                     @Override
-                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
-                        viewModel.setDate(new GregorianCalendar(i, i1, i2).getTime());
+                    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                        int ini1 = Integer.parseInt(viewModel.getHoraInicio().getValue().split(":")[0]);
+                        int ini2 = Integer.parseInt(viewModel.getHoraInicio().getValue().split(":")[1]);
+                        Calendar cIni = Calendar.getInstance();
+                        cIni.set(Calendar.HOUR_OF_DAY, ini1);
+                        cIni.set(Calendar.MINUTE, ini2);
+                        Calendar cFin = Calendar.getInstance();
+                        cFin.set(Calendar.HOUR_OF_DAY, i);
+                        cFin.set(Calendar.MINUTE, i1);
+                        if(cFin.before(cIni)){
+                            cFin.add(Calendar.DAY_OF_MONTH,1);
+                        }
+                        long diferenciaMillis = cFin.getTimeInMillis() - cIni.getTimeInMillis();
+                        long diferenciaHoras = diferenciaMillis / (1000 * 60 * 60);
+                        long diferenciaMinutos = (diferenciaMillis / (1000 * 60)) % 60;
+
+                        if (diferenciaHoras < 9 || (diferenciaHoras == 9 && diferenciaMinutos <= 0)) {
+                            viewModel.setHoraFin(String.format("%02d:%02d", i, i1));
+                        } else {
+                            Toast.makeText(getContext(), "La reserva puede ser máximo de 9 horas", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 };
-                DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), listener, year, mes, dia);
-                datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
-                Calendar maxDate = Calendar.getInstance();
-                maxDate.add(Calendar.DAY_OF_MONTH, 7);
-                datePickerDialog.getDatePicker().setMaxDate(maxDate.getTimeInMillis());
-                datePickerDialog.show();
+                TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(), listener, hora, minute, true);
+                timePickerDialog.show();
             }
         });
 
         viewModel.getDate().observe(getViewLifecycleOwner(), date -> {
             SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
             binding.tvDia.setText(format.format(date));
+        });
+
+        viewModel.getHoraInicio().observe(getViewLifecycleOwner(), horaInicio -> {
+            binding.tvHoraInicio.setText(horaInicio);
+        });
+
+        viewModel.getHoraFin().observe(getViewLifecycleOwner(), horaFin -> {
+            binding.tvHoraSalida.setText(horaFin);
         });
     }
 }
--- app/src/main/java/com/lksnext/ParkingELadron/viewmodel/CrearViewModel.java
+++ app/src/main/java/com/lksnext/ParkingELadron/viewmodel/CrearViewModel.java
@@ -11,11 +11,31 @@ import java.util.Date;
 public class CrearViewModel extends ViewModel {
     MutableLiveData<Date> date = new MutableLiveData<Date>();
 
+    MutableLiveData<String> horaInicio = new MutableLiveData<String>();
+
+    MutableLiveData<String> horaFin = new MutableLiveData<String>();
+
     public LiveData<Date> getDate() {
         return date;
     }
 
     public void setDate(Date d) {
         this.date.setValue(d);
+    }
+
+    public LiveData<String> getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(String horaInicio) {
+        this.horaInicio.setValue(horaInicio);
+    }
+
+    public LiveData<String> getHoraFin() {
+        return horaFin;
+    }
+
+    public void setHoraFin(String horaFin) {
+        this.horaFin.setValue(horaFin);
     }
 }
